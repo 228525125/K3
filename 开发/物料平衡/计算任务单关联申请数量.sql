ab4937c7476945c01e4a77855993c72e6b72362d
chenxian <228525125@qq.com>
2015-03-07 13:46:28 +0800

    报废规则变化

--- 开发/物料平衡/计算任务单关联申请数量.sql
+++ 开发/物料平衡/计算任务单关联申请数量.sql
@@ -12,25 +12,27 @@ FBillNo nvarchar(20) default('')
 Insert Into #temp(FBillNo,fssl
 )
 SELECT a.FBillNo,
-max(a.FQty - (ROUND(case when b.FAuxStockQty>b.FAuxQtyMust then b.FAuxQtyMust else b.FAuxStockQty end/b.FAuxQtyScrap,0) - ROUND(b.FDiscardAuxQty/b.FAuxQtyScrap,0) + ISNULL(-e.FQty,0) - ISNULL(c.FQty,0))) as '计算结果'
+max(a.FQty - (ROUND(case when (b.FAuxStockQty + ISNULL(-f.FQty,0) + ISNULL(-k.FQty,0))>b.FAuxQtyMust then b.FAuxQtyMust else (b.FAuxStockQty + ISNULL(-f.FQty,0) + ISNULL(-k.FQty,0)) end/b.FAuxQtyScrap,0) - ROUND(b.FDiscardAuxQty/b.FAuxQtyScrap,0) + ISNULL(-e.FQty,0) - ISNULL(c.FQty,0))) as '计算结果'
 FROM ICMO a
 INNER JOIN PPBOMEntry b on b.FICMOInterID=a.FInterID
 LEFT JOIN (SELECT a.FICMOInterID,sum(b.FQty) as FQty FROM QMICMOCKRequest a INNER JOIN QMICMOCKRequestEntry b on a.FInterID=b.FInterID group by a.FICMOInterID) c on c.FICMOInterID=a.FInterID
 LEFT JOIN t_ICItem d on b.FItemID=d.FItemID
 LEFT JOIN (select b.FICMOInterID,b.FPPBomEntryID,sum(b.FQty) as FQty from ICStockBill a inner join ICStockBillEntry b on a.FInterID=b.FInterID where a.FTranType=24 AND a.FCancellation = 0 AND a.FStatus = 1 AND a.FROB=-1 and a.FDate<'2013-06-26' and b.FSCStockID=5272 group by b.FICMOInterID,b.FPPBomEntryID) e on e.FICMOInterID=a.FInterID and b.FEntryID=e.FPPBomEntryID
-LEFT JOIN t_ICItem f on a.FItemID=f.FItemID
+LEFT JOIN (select b.FICMOInterID,b.FPPBomEntryID,sum(b.FQty) as FQty from ICStockBill a inner join ICStockBillEntry b on a.FInterID=b.FInterID where a.FTranType=24 AND a.FCancellation = 0 AND a.FStatus = 1 AND a.FROB=-1 and b.FSCStockID<>5272 group by b.FICMOInterID,b.FPPBomEntryID) f on f.FICMOInterID=a.FInterID and b.FEntryID=f.FPPBomEntryID
+LEFT JOIN (select b.FICMOInterID,b.FPPBomEntryID,sum(b.FQty) as FQty from ICSTJGBill a inner join ICSTJGBillEntry b on a.FInterID=b.FInterID where a.FTranType=137 AND a.FCancellation = 0 AND a.FStatus=1 AND a.FROB=-1 and b.FSCStockID=5766 group by b.FICMOInterID,b.FPPBomEntryID) k on k.FICMOInterID=a.FInterID and b.FEntryID=k.FPPBomEntryID
+LEFT JOIN t_ICItem i on a.FItemID=i.FItemID
 where 1=1
 and b.FAuxQtyMust>0                  --计划投料数量等于0 表示取消投料，因此不参加计算
 --and not exists(select 1 from PPBOM bom inner join PPBOMEntry bome on bom.FInterID=bome.FInterID where bome.FICMOInterID=b.FICMOInterID and bome.FEntryID=2 and bom.FItemID=a.FItemID)   --不存在产品bom里有多于1个原材料，即成品与原材料一一对应，bom没有多于的原材料
 and left(d.FNumber,3)<>'08.'                        --不考虑包装材料
 and b.FAuxQtyScrap > 0                              --单位用量必须大于0
 --and b.FUnitID in (179,181,183,185,187,189,214,227,334,338,5947)           --计量单位没有小数点的
-and a.FQty -(ROUND(case when b.FAuxStockQty>b.FAuxQtyMust then b.FAuxQtyMust else b.FAuxStockQty end/b.FAuxQtyScrap,0) - ROUND(b.FDiscardAuxQty/b.FAuxQtyScrap,0) + ROUND(ISNULL(-e.FQty,0)/b.FAuxQtyScrap,0) - ISNULL(c.FQty,0)) <> a.FCheckCommitQty   --计划 - (已领 - 报废 + 退料（2013-06-26之前） - 申请)
+and a.FQty -(ROUND(case when (b.FAuxStockQty + ISNULL(-f.FQty,0) + ISNULL(-k.FQty,0))>b.FAuxQtyMust then b.FAuxQtyMust else (b.FAuxStockQty + ISNULL(-f.FQty,0) + ISNULL(-k.FQty,0)) end/b.FAuxQtyScrap,0) - ROUND(b.FDiscardAuxQty/b.FAuxQtyScrap,0) + ROUND(ISNULL(-e.FQty,0)/b.FAuxQtyScrap,0) - ISNULL(c.FQty,0)) <> a.FCheckCommitQty   --计划 - (已领 + 退料（2015-02-01之后） - 报废 + 退料（2013-06-26之前） - 申请)
 --and a.FQty -(ROUND(case when b.FAuxStockQty>b.FAuxQtyMust then b.FAuxQtyMust else b.FAuxStockQty end/b.FAuxQtyScrap,0) - ROUND(b.FDiscardAuxQty/b.FAuxQtyScrap,0) + ROUND(ISNULL(-e.FQty,0)/b.FAuxQtyScrap,0) - ISNULL(c.FQty,0)) >= 0       --计算结果为负数，因为有多领料的情况；2013-09-27被注释，因为这个条件会导致FCheckCommitQty不会更新
 and a.FStatus in (1,2)        --下达状态
-and f.FProChkMde=353         --产品检验方式为抽检
+and i.FProChkMde=353         --产品检验方式为抽检
 and a.FCheckDate>='2014-01-01'   --之前的时间是2013-06-01，之所以修改时间是因为之前不考虑多部件的情况
-and f.FNumber <>'05.03.0105'
+and i.FNumber <>'05.03.0105'
 and d.FNumber <> '06.07.0135' and d.FNumber<>'06.07.0045' and d.FNumber<>'06.07.0040' and d.FNumber<>'06.04.0049' and d.FNumber<>'06.07.0111' and d.FNumber <>'06.07.0108' and d.FNumber <>'06.07.0107'
 and a.FBillNo<>'WORK027512'   --问题单据
 and a.FBillNo<>'WORK028370'
@@ -175,3 +177,34 @@ delete thcx where id=7299
 
 
 
+
+SELECT a.FBillNo,
+a.FQty,
+ROUND(case when (b.FAuxStockQty + ISNULL(-f.FQty,0) + ISNULL(-k.FQty,0))>b.FAuxQtyMust then b.FAuxQtyMust else b.FAuxStockQty end/b.FAuxQtyScrap,0),
+ROUND(b.FDiscardAuxQty/b.FAuxQtyScrap,0), 
+ROUND(ISNULL(-e.FQty,0)/b.FAuxQtyScrap,0) , 
+ISNULL(c.FQty,0) , 
+a.FCheckCommitQty,
+a.FQty -(ROUND(case when (b.FAuxStockQty + ISNULL(-f.FQty,0) + ISNULL(-k.FQty,0))>b.FAuxQtyMust then b.FAuxQtyMust else (b.FAuxStockQty + ISNULL(-f.FQty,0) + ISNULL(-k.FQty,0)) end/b.FAuxQtyScrap,0) - ROUND(b.FDiscardAuxQty/b.FAuxQtyScrap,0) + ROUND(ISNULL(-e.FQty,0)/b.FAuxQtyScrap,0) - ISNULL(c.FQty,0))
+--max(a.FQty - (ROUND(case when b.FAuxStockQty>b.FAuxQtyMust then b.FAuxQtyMust else b.FAuxStockQty end/b.FAuxQtyScrap,0) - ROUND(b.FDiscardAuxQty/b.FAuxQtyScrap,0) + ISNULL(-e.FQty,0) - ISNULL(c.FQty,0))) as '计算结果'
+FROM ICMO a
+INNER JOIN PPBOMEntry b on b.FICMOInterID=a.FInterID
+LEFT JOIN (SELECT a.FICMOInterID,sum(b.FQty) as FQty FROM QMICMOCKRequest a INNER JOIN QMICMOCKRequestEntry b on a.FInterID=b.FInterID group by a.FICMOInterID) c on c.FICMOInterID=a.FInterID
+LEFT JOIN t_ICItem d on b.FItemID=d.FItemID
+LEFT JOIN (select b.FICMOInterID,b.FPPBomEntryID,sum(b.FQty) as FQty from ICStockBill a inner join ICStockBillEntry b on a.FInterID=b.FInterID where a.FTranType=24 AND a.FCancellation = 0 AND a.FStatus = 1 AND a.FROB=-1 and a.FDate<'2013-06-26' and b.FSCStockID=5272 group by b.FICMOInterID,b.FPPBomEntryID) e on e.FICMOInterID=a.FInterID and b.FEntryID=e.FPPBomEntryID
+LEFT JOIN (select b.FICMOInterID,b.FPPBomEntryID,sum(b.FQty) as FQty from ICStockBill a inner join ICStockBillEntry b on a.FInterID=b.FInterID where a.FTranType=24 AND a.FCancellation = 0 AND a.FStatus = 1 AND a.FROB=-1 and b.FSCStockID<>5272 group by b.FICMOInterID,b.FPPBomEntryID) f on f.FICMOInterID=a.FInterID and b.FEntryID=f.FPPBomEntryID
+LEFT JOIN (select b.FICMOInterID,b.FPPBomEntryID,sum(b.FQty) as FQty from ICSTJGBill a inner join ICSTJGBillEntry b on a.FInterID=b.FInterID where a.FTranType=137 AND a.FCancellation = 0 AND a.FStatus=1 AND a.FROB=-1 and b.FSCStockID=5766 group by b.FICMOInterID,b.FPPBomEntryID) k on k.FICMOInterID=a.FInterID and b.FEntryID=k.FPPBomEntryID
+--LEFT JOIN t_ICItem f on a.FItemID=f.FItemID
+where 1=1
+and b.FAuxQtyMust>0                  --计划投料数量等于0 表示取消投料，因此不参加计算
+and left(d.FNumber,3)<>'08.'                        --不考虑包装材料
+and b.FAuxQtyScrap > 0                              --单位用量必须大于0
+and a.FQty -(ROUND(case when (b.FAuxStockQty + ISNULL(-f.FQty,0) + ISNULL(-k.FQty,0))>b.FAuxQtyMust then b.FAuxQtyMust else (b.FAuxStockQty + ISNULL(-f.FQty,0) + ISNULL(-k.FQty,0)) end/b.FAuxQtyScrap,0) - ROUND(b.FDiscardAuxQty/b.FAuxQtyScrap,0) + ROUND(ISNULL(-e.FQty,0)/b.FAuxQtyScrap,0) - ISNULL(c.FQty,0)) <> a.FCheckCommitQty   --计划 - (已领 - 报废 + 退料（2013-06-26之前） - 申请)
+and a.FStatus in (1,2)        --下达状态
+and a.FCheckDate>='2015-01-01'   --之前的时间是2013-06-01，之所以修改时间是因为之前不考虑多部件的情况
+and a.FBillNo='WORK036061'
+--group by a.FBillNo
+
+
+
+select * FROM ICMO a
